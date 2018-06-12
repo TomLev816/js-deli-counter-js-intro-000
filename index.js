2ae0a65db3cbe546e0d5cace78d02b2f64ccce40
Thomas Lever <thomaslever508@gmail.com>
2018-06-12 15:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeli,name){
   katzDeli.push(name)
   return 'Welcome, ' + name + '. You are number ' + (katzDeli.length) + ' in line.'
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.Length > 0){
+    return 'Currently Serving ' + katzDeliLine[0]
+    katzDeliLine.shift()
+  } else {
+    return 'There is nobody waiting to be served'
+  }
+}
