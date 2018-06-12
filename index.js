89d1c1cddd789820efa9221b752ea47eb16205af
Thomas Lever <thomaslever508@gmail.com>
2018-06-12 15:13:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, name){
+  for (let i = 1; i < name.length +1; i++){
+    katzDeliLine.push(name)
+    return 'Welcome, ' + name + '. You are number ' i + ' in line.'
+  }
+}
+
+function nowServing(katzDeliLine){
+    for (let i = 0; i < katzDeliLine.length; i++){
+      return 'Currently serving' + katzDeliLine[i]
+      katzDeliLine.shift()
+    }
+}
+
+function currentLine(katzDeliLine){
+  for (let i = 0; i < katzDeliLine.length; i++){
+    return 'The line is currently ' + i+1 + '. ' + katzDeliLine[i] 
+  }
+  return 'The line is currently empty.'
+}
