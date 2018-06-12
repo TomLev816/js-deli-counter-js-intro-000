905172223fed32fa8e9f09e85c7a590ad25918bb
Thomas Lever <thomaslever508@gmail.com>
2018-06-12 15:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   for (let i = 0; i < katzDeliLine.length; i++){
-    return 'The line is currently ' + i+1 + '. ' + katzDeliLine[i] 
+    return 'The line is currently ' + i+1 + '. ' + katzDeliLine[i]
   }
   return 'The line is currently empty.'
 }
