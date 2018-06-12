d39d1dfba643c60da0c8152e9343fc10eb9554d8
Thomas Lever <thomaslever508@gmail.com>
2018-06-12 16:15:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function nowServing(katzDeliLine){
     return 'There is nobody waiting to be served'
   }
 }
+
+function currentLine(line){
+  if (line.length === 0){
+    return 'The line is currently empty.'
+  } else {
+    let theLine = []
+    for (let i = 0; i < line.length; i++){
+      theLine.push(' ' + (i+1) + '. ' + line[i])
+    }
+return 'The line is currently:' + theLine 
+  }  
+}
